bed63e83fdfe67ae1d886242e3229126c35811ec
Boxer2211 <yakovets.champ@gmail.com>
2022-03-19 15:45:48 +0200

    sdfa

--- javascript.js
+++ javascript.js
@@ -2,85 +2,85 @@
 // * Bиведіть в консоль тип змінної використовуючи оператор "typeof" і функцію "console.log()"
 
 let a = 1;
-let b = String(a);
-console.log(typeof b);
+a = String(a);
+console.log(typeof a, a);
 
 // 2) Оголосіть змінну типу number і зробіть явне перетворення до типу boolean використовуючи “Boolean()”
 // * Bиведіть в консоль тип змінної використовуючи оператор "typeof" і функцію "console.log()"
 
 let aa = 0;
-let bb = Boolean(aa);
-console.log(typeof bb, bb);
+aa = Boolean(aa);
+console.log(typeof aa, aa);
 
 // 3) Оголосіть змінну типу number і зробіть явне перетворення до типу null використовуючи “Null()”
 // * Bиведіть в консоль тип змінної використовуючи оператор "typeof" і функцію "console.log()"
 
 let aaa = 22;
-let bbb = (a = null); // вимушений так зробити, бо писало в консолі браузера, що Null not defined. 
-console.log(typeof bbb, bbb); // І далі так робив, а не через функцію.
+aaa = (aaa = null);           // вимушений так зробити, бо писало в консолі браузера, що Null not defined. 
+console.log(typeof aaa, aaa); // І далі так робив
 
 // 4) Оголосіть змінну типу string і зробіть явне перетворення до типу number використовуючи “Number()”
 // * Bиведіть в консоль тип змінної використовуючи оператор "typeof" і функцію "console.log()"
 
 let ilon = '22';
-let numilon = Number(ilon);
-console.log(typeof numilon, numilon);
+ilon = Number(ilon);
+console.log(typeof ilon, ilon);
 
 // 5) Оголосіть змінну типу string і зробіть явне перетворення до типу boolean використовуючи “Boolean()”
 // * Bиведіть в консоль тип змінної використовуючи оператор "typeof" і функцію "console.log()"
 
 let str = 'i am batman';
-let boolstr = Boolean(str);
-console.log(typeof boolstr, boolstr);
+str = Boolean(str);
+console.log(typeof str, str);
 
 // 6) Оголосіть змінну типу string і зробіть явне перетворення до типу null використовуючи “Null()”
 // * Bиведіть в консоль тип змінної використовуючи оператор "typeof" і функцію "console.log()"
 
 let wot = 'Allisov';
-let nulwot = (wot = null);
-console.log(typeof nulwot, nulwot);
+wot = (wot = null);
+console.log(typeof wot, wot);
 
 // 7) Оголосіть змінну типу boolean і зробіть явне перетворення до типу string використовуючи “String()”
 // * Bиведіть в консоль тип змінної використовуючи оператор "typeof" і функцію "console.log()"
 
 let ball = true;
-let strball = String(ball);
-console.log(typeof strball, strball);
+ball = String(ball);
+console.log(typeof ball, ball);
 
 // 8) Оголосіть змінну типу boolean і зробіть явне перетворення до типу number використовуючи “Number()”
 // * Bиведіть в консоль тип змінної використовуючи оператор "typeof" і функцію "console.log()"
 
 let ball1 = false;
-let strball1 = Number(ball1);
-console.log(typeof strball1, strball1);
+ball1 = Number(ball1);
+console.log(typeof ball1, ball1);
 
 // 9) Оголосіть змінну типу boolean і зробіть явне перетворення до типу null використовуючи Null()”
 // * Bиведіть в консоль тип змінної використовуючи оператор "typeof" і функцію "console.log()"
 
 let ball2 = true;
-let strball2 = (ball2 = null);
-console.log(typeof strball2, strball2);
+ball2 = (ball2 = null);
+console.log(typeof ball2, ball2);
 
 // 10) Оголосіть змінну типу null і зробіть явне перетворення до типу string використовуючи “String()”
 // * Bиведіть в консоль тип змінної використовуючи оператор "typeof" і функцію "console.log()"
 
 let test = null;
-let strtest = String(test);
-console.log(typeof strtest, strtest);
+test = String(test);
+console.log(typeof test, test);
 
 // 11) Оголосіть змінну типу null і зробіть явне перетворення до типу number використовуючи “Number()”
 // * Bиведіть в консоль тип змінної використовуючи оператор "typeof" і функцію "console.log()"
 
 let test1 = null;
-let strtest1 = Number(test1);
-console.log(typeof strtest1, strtest1);
+test1 = Number(test1);
+console.log(typeof test1, test1);
 
 // 12) Оголосіть змінну типу null і зробіть явне перетворення до типу boolean використовуючи Boolean()”
 // * Bиведіть в консоль тип змінної використовуючи оператор "typeof" і функцію "console.log()"
 
 let test2 = null;
-let strtest2 = Boolean(test2);
-console.log(typeof strtest2, strtest2);
+test2 = Boolean(test2);
+console.log(typeof test2, test2);
 
 // 13) Напишіть всі способи створення функції.
 
@@ -109,29 +109,34 @@ console.log(func1());
 // * Викличіть функцію у двох різних варіантах, з даними aбо із змінними.
 
 function one(arg1) {
-	console.log(arg1);
+	
 	return arg1;
 }
-console.log(one(55));
+let artem = 20;
+console.log(one(55), one(artem));
 
 
 // 17) Створіть функцію яка приймає 2 аргументи і повертає суму цих 2 аргументів.
 // * Викличіть функцію у двох різних варіантах, з даними aбо із змінними.
 
 function one1(num1, num2) {
-	console.log(num1, num2);
-	return num1 + num2;
+		return num1 + num2;
 }
-console.log(one1(5,7));
+let andr = 3;
+let nik = 4;
+console.log(one1(5,7), one1(andr,nik));
 
 // 18) Створіть функцію яка приймає 3 аргументи і повертає суму цих 3 аргументів.
 // * Викличіть функцію у двох різних варіантах, з даними aбо із змінними.
 
 function one2(nume1, nume2, nume3) {
-	console.log(nume1, nume2, nume3);
+	
 	return nume1 + nume2 + nume3;
 }
-console.log(one2(5,7,8));
+let st = 1;
+let pt = 2;
+let lt = 3;
+console.log(one2(5,7,8), one2(st,pt,lt));
 
 
 function one3(numer1, numer2, numer3) {
@@ -146,3 +151,14 @@ function func15(name = 'Аноним', age) {
 	alert('ваше имя: ' + String(name) + ' вік ' + age);
 }
 func15('dd',13);
+
+
+function sumsqr(arr) {
+	let a = [];
+	for(let i = 0; i <= arr; i++) {
+		;
+	}
+	return a;
+}
+let arr1 = 5
+alert(sumsqr(arr1));
