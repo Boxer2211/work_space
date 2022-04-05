e644cfd2cae184bee7768c81297137a83748572a
Boxer2211 <yakovets.champ@gmail.com>
2022-04-05 16:04:43 +0300

    1

---
+++ Practic/Practic.js
@@ -0,0 +1,26 @@
+// Дан инпут и кнопка. В инпут вводится число. По нажатию на кнопку выведите список делителей этого числа.
+
+/*
+let input = document.querySelector('#first');
+let button = document.querySelector('#button');
+let result = document.querySelector('#result');
+let arr = [];
+
+button.addEventListener('click', func = () => {
+                        let va = input.value; 
+                            for(let i = 1; i <= va; i++) {
+                               if (va % i == 0) {
+                               arr.push(i);
+                               console.log(arr);
+                                                       
+                                    }
+                               }
+                          result.innerHTML = arr;
+ }
+)
+*/
+
+
+
+
+
