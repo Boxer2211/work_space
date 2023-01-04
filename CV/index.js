2eb6836b1b664e2e0f0552309918bb13c357f847
Boxer2211 <yakovets.champ@gmail.com>
2023-01-04 22:29:28 +0200

    cv

---
+++ CV/index.js
@@ -0,0 +1,11 @@
+// const developer = {
+//     name: "Olexandr Yakivets",
+//     yearsOfExperience: 1,
+//     skills: {
+//         frontend: ["React", "Redux toolkit", "REST API", "HTML", "CSS", "JavaScript", "TypeScript"],
+//         backend: ["Node.js", "Express", "MOngoDB", "Firebase"],
+//     },
+//     lookingForWork: true
+// }
+
+
