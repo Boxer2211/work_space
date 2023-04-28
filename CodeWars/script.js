d13dc25c83b3163d0f425dcda8c6784da5b0ebaa
Alexandr <yakovets.champ@gmail.com>
2023-04-28 15:06:50 +0300

    1

--- CodeWars/script.js
+++ CodeWars/script.js
@@ -1,36 +1,10 @@
-function circleSlash(n) {
-    function todo2 (arr){
-            for(i = 0; i < arr.length; i++){
-                arr.splice(i+1,1)
-              }
-             
-              return arr.length > 1 ? todo2(arr) : arr[0]
-            }
-            
-    function todo1 (arr){ 
-            for(i = 0; i < arr.length; i++){
-                arr.splice(i+1,1)
-              }
-              console.log(arr);
-              if(arr.length > 1){
-                arr.shift()  
-                return todo1(arr);
-              } else {
-                  return arr[0]
-              } 
-        }
-    let result = [];
-    if (n === 1){
-      return 1;
-    } else {
-      for (let i = 1; i <= n; i++){
-        result.push(i)
-      }
-      return result[result.length-1] % 2 === 0 ? todo2(result) : todo1(result)
-    }
-  }
 
-  console.log(circleSlash(83))
+ const fetchProduct = async () => {
+  const {data} = await https://lukon-dev.fairtech-marketing.com/api/v1/
+  return data;
+}
+
+fetchProduct()
 
   
   
