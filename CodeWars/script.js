0c8375dc0a99b874037fcd9a3c353923a68ca812
Alexandr <yakovets.champ@gmail.com>
2023-06-19 15:33:40 +0300

    1

--- CodeWars/script.js
+++ CodeWars/script.js
@@ -1,13 +1,21 @@
+const array = [1, 2, 3, [4, 5], null, 6, 'name', { one: 'one' }];
 
-const developer = {
-  name: "Oleksandr Yakivets",
-  yearsOfExperience: "01 June 2022 - for now",
-  skills: {
-    frontend: ["React", "Redux", "Rest API", "HTML", "CSS", "JS", "TS"],
-    backend: ["Node.js", "Express", "MongoDB", "Firebase"],
-    other: ["GIT version control", "Pixel perfect"]
-  }
-}
+/** Don't use `for` and `while` loops */
+const sumAllNumbersInArray = (value, sum = 0) => {
+ return sum = value.reduce((accum, val) => {
+    if(Array.isArray(val)){
+      return accum + (val.reduce((a, v) => a + v ))
+     } else if (typeof val === 'number'){
+      return accum + val
+     } else {
+      return accum
+     }
+      
+  }, 0);
 
+  // console.log(sum);
+};
+
+console.info(sumAllNumbersInArray(array)); // 21 ( i.e. 1 + 2 + 3 + 4 + 5 + 6 )
 
 
