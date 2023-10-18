4f80f6d6495053fd28b0151678fffc7db8dd238c
Alexandr <yakovets.champ@gmail.com>
2023-10-18 14:06:32 +0300

    тестове старт

---
+++ CodeWars/script.js
@@ -0,0 +1,9 @@
+function shiftedDiff(first,second){
+    first = first.split('')
+    second = second.split('')
+    for (let i = 0; i < first.length; i++){
+      if (first.includes(second[i])){
+        return -1
+    }
+    console.log(first)
+  }
